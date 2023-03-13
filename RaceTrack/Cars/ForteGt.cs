347db995012ac795e578383c8c29d68a54b80a5b
Johnathan Delfosse <jdelfosse@my.wctc.edu>
2023-03-13 18:41:56 -0500

    Finished, with StopEngine() added and implemented.

---
+++ RaceTrack/Cars/ForteGt.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceTrack.RaceTrack.Cars
+{
+    public class ForteGt : RaceCar
+    {
+        public ForteGt()
+        {
+            Name = "Forte GT";
+            TopSpeed = 85;
+        }
+
+        public override void StartEngine()
+        {
+            Console.WriteLine($"The {Name} starts with a low rumble!");
+        }
+ 
+
+        public override void Brake()
+        {
+            Console.WriteLine($"The {Name} slows way down!");
+            base.Brake();
+        }
+
+        public override void StopEngine()
+        {
+            Console.WriteLine($"The {Name}'s engine shuts down with a low whoosh!");
+        }
+    }
+}
